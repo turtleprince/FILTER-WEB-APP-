2a5c00049fd251d0023b9006f4ae980e339861f8
turtleprince <81336142+turtleprince@users.noreply.github.com>
2022-05-31 19:55:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+
+}
+
+function setup(){
+        
+    canvas = createCanvas(309,309);
+    canvas.center();
+
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("rick+roll.png");
+}
+
